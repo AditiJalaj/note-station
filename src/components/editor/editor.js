68d972d8ed888901d77ce67ab2b958fc4073fdf3
ajalaj <aditi.jalaj@gmail.com>
2021-06-06 14:46:56 +0530

    debounce logic implemented with useEffect

--- src/components/editor/editor.js
+++ src/components/editor/editor.js
@@ -3,19 +3,29 @@ import styles from './editorstyles.js'
 import BorderColorIcon from '@material-ui/icons/BorderColor'
 import ReactQuill from 'react-quill'
 import {useState,useEffect} from 'react'
-import debounce from '../../helpers'
-import { compose } from 'redux'
+// import debounce from '../../helpers'
+
+
 
 const Editor = ({}) => {
     const [title,setTitle]=useState('')
     const [text,setText]=useState('')
     const [id,setId]=useState('')
 
+ 
+
+    useEffect(() => {
+        let timeout = setTimeout(() => {
+          console.log("updating db ", text);
+        }, 1500);
+        
+        // this clean up function  in next line makes sure we update data in firebase only when user stops typing for 1.5 sec
+        return () => clearTimeout(timeout);
+      }, [text]);
+
+
     function updateText(val){
         setText(val)
-        console.log(text)
-
-        //implement debounce logic here
     }
     
     return ( <div className="editorContainer">
